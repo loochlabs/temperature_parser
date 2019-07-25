43d4e81ef531acc1a515a5b16c5cb69cde61c16c
Steve Coluccio <thecalooch@gmail.com>
2019-07-25 03:37:22 -0400

    replace function, more command line options

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # temperature_parser
+
+This python script is meant for parsing CSV files with a CSV file output
+
+Commandline arguements
+>python parse.py <flag> <input csv> <output csv> <min> <max>
+
+Flags
+*-p : purge values in given range
+*-r : replace values in given range with NaN   
--- parse.py
+++ parse.py
@@ -1,6 +1,6 @@
 import sys
 
-def parse(i, o, mn, mx):
+def purge(i, o, mn, mx):
 	infile = open(i)
 	outfile = open(o, "w")
 	arr = infile.read().split(',')
@@ -15,13 +15,40 @@ def parse(i, o, mn, mx):
 	infile.close()
 	outfile.close()
 
-infile = sys.argv[1]
-outfile = sys.argv[2]
-minRange = sys.argv[3]
-maxRange = sys.argv[4]
+def replace(i, o, mn, mx):
+	infile = open(i)
+	outfile = open(o, "w")
+	arr = infile.read().split(',')
+	outstr = ""
+	for value in arr :
+		if int(mn) < int(value) < int(mx) :
+			outstr += value + ","
+		else :
+			outstr += "NaN,"
+
+	outstr = outstr[:-1]
+	outfile.write(outstr)
+
+	infile.close()
+	outfile.close()
+
+if(len(sys.argv)) != 6 :
+	raise Exception('invalid arguements')
+
+flag = sys.argv[1]
+
+infile = sys.argv[2]
+outfile = sys.argv[3]
+minRange = sys.argv[4]
+maxRange = sys.argv[5]
 
 log = "Parsing this fancy data between "
 log += "[" + str(minRange) + "-" + str(maxRange) + "]"
 print(log) 
 
-parse(infile, outfile, minRange, maxRange)
+if flag == "-p" :
+	purge(infile, outfile, minRange, maxRange)
+elif flag == "-r" :
+	replace(infile, outfile, minRange, maxRange)
+else :
+	raise Exception('invalid flag arguement : {}'.format(flag))
