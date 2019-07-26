3472bcc93abbbe9923a0f0eb586966874ed10a66
Steve Coluccio <thecalooch@gmail.com>
2019-07-26 07:59:03 -0400

    refining mask, adding some real data

--- mask.py
+++ mask.py
@@ -7,14 +7,36 @@ mn = int(sys.argv[3])
 mx = int(sys.argv[4])
 
 infile = open(inname)
-arr = list(map(int, infile.read().split(',')))
-infile.close()
-arr = np.array(arr)
-mask = arr < mx
+
+#remove first line
+with open(inname) as f :
+	content = f.readlines()
+
+content = content[1:]
+content = [x.strip() for x in content]
+content = [x.strip('\n') for x in content]
+
+arr = []
+for n in content :
+	for val in n.split(',') :
+		arr.append(float(val))
+
+
+#arr = list(map(int, content.split(',')))
+#infile.close()
+arr = np.array(list(map(float,arr)))
+mask = (arr < mx) & (arr > mn)
 
 outarray = []
+minRead = 1000
+maxRead = 0
 for n in range(len(arr)) :
 	outarray.append("NaN" if not mask[n] else arr[n])
+
+	minRead = min(minRead, arr[n])
+	maxRead = max(maxRead, arr[n])
+
+print("Min value {0}, Max value {1}".format(minRead, maxRead))
 
 outfile = open(outname, "w")
 outstr = str(outarray)[1:-1] #strip [] from the stringified array
