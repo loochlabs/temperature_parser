583c0d65c95ba5c2ddf36580b7847d45ac3e6140
Steve Coluccio <thecalooch@gmail.com>
2019-07-26 16:14:14 -0400

    tiff converter

---
+++ converter.py
@@ -0,0 +1,118 @@
+#csv to tif
+from PIL import Image
+import numpy as np
+import sys
+import os
+
+
+def temp_to_b(pct) :
+	tmin = 0
+	tmid1 = 0.1667
+	tmid2 = 0.3333
+	tmax = 0.5
+
+	if tmin < pct < tmid1 :
+		return int(255*(pct/tmid1)) #[0-255]
+
+	if tmid1 < pct < tmid2 :
+		return 255
+
+	if tmid2 < pct < tmax :
+		return 255 - int(255*(pct/tmid1)) #[255-0]
+
+	return 0
+
+def temp_to_g(pct) :
+	tmin = 0.1667
+	tmid1 = 0.3333
+	tmid2 = 0.6667
+	tmax = 0.8333
+
+	if tmin < pct < tmid1 :
+		return int(255*(pct/tmid1)) #[0-255]
+
+	if tmid1 < pct < tmid2 :
+		return 255
+
+	if tmid2 < pct < tmax :
+		return 255 - int(255*(pct/tmid1)) #[255-0]
+
+	return 0
+
+def temp_to_r(pct) :
+	tmin = 0.5
+	tmid1 = 0.6667
+	tmid2 = 0.8333
+	tmax = 1.0
+
+	if tmin < pct < tmid1 :
+		return int(255*(pct/tmid1)) #[0-255]
+
+	if tmid1 < pct < tmid2 :
+		return 255
+
+	if tmid2 < pct < tmax :
+		return 255 - int(255*(pct/tmid1)) #[255-0]
+
+	return 0
+
+def csv_to_tiff(filename, mn, mx) :
+	print("Converting {}...".format(filename))
+
+	with open(filename) as f :
+		content = f.readlines()
+
+	dimensions = content[0]
+	dimensions = dimensions.split(",")
+	for n in range(len(dimensions)) :
+		dimensions[n] = dimensions[n].strip('\n')
+
+	content = content[1:]
+	contentArr = []
+
+	#create an NxMx3 rgb matrix for each csv entry
+	rgbArray = np.zeros((int(dimensions[0]), int(dimensions[1]), 3), 'uint8')
+
+	#grab all content and store in 1d array of floats
+	tempMin = float(argMinTemp)
+	tempMax = float(argMaxTemp)
+	tempDiff = float(tempMax - tempMin) 
+	arr = []
+	for y in range(len(content)) :
+		row = content[y].split(',')
+		for x in range(len(row)) :
+			if tempMin < float(row[x]) < tempMax : 
+				rgbArray[x][y][0] = temp_to_r(1 - ((tempMax - float(row[x])) / tempDiff)) 
+				rgbArray[x][y][1] = temp_to_g(1 - ((tempMax - float(row[x])) / tempDiff))  
+				rgbArray[x][y][2] = temp_to_b(1 - ((tempMax - float(row[x])) / tempDiff))  
+
+	data32 = np.array(rgbArray)
+	np.clip(data32, 0, 255, out=data32)
+	data_u8 = data32.astype('uint8')
+
+	im = Image.fromarray(data_u8)
+
+	outfile = filename.split('.', 1)[0]
+
+	im.save(outfile + '.tif', 'TIFF')
+
+
+argflag = sys.argv[1]
+inname = sys.argv[2]
+
+argMinTemp = 0
+if len(sys.argv) > 3 :
+	argMinTemp = sys.argv[3]
+
+argMaxTemp = 100
+if len(sys.argv) > 4 :
+	argMaxTemp = sys.argv[4]
+
+#single file
+if argflag == "-f" :
+	csv_to_tiff(inname, argMinTemp, argMaxTemp)
+
+if argflag == "-d" :
+	for f in os.listdir(inname) :
+		if ".csv" in f :
+			csv_to_tiff(inname + "/" + f, argMinTemp, argMaxTemp)
--- mask.py
+++ mask.py
@@ -12,26 +12,44 @@ infile = open(inname)
 with open(inname) as f :
 	content = f.readlines()
 
+dimensions = content[0]
+dimensions = dimensions.split(",")
+for n in range(len(dimensions)) :
+	dimensions[n] = dimensions[n].strip('\n')
+
 content = content[1:]
 content = [x.strip() for x in content]
 content = [x.strip('\n') for x in content]
 
+#grab all content and store in 1d array of floats
 arr = []
 for n in content :
 	for val in n.split(',') :
 		arr.append(float(val))
 
 
-#arr = list(map(int, content.split(',')))
-#infile.close()
+#convert to numpy array, mask values in range
 arr = np.array(list(map(float,arr)))
 mask = (arr < mx) & (arr > mn)
 
-outarray = []
+outstr = ""
+#outstr = dimensions[0] + "," + dimensions[1] + "\n"
+print(outstr)
+
+rowstr = ""
 minRead = 1000
 maxRead = 0
+currentCol = 0
 for n in range(len(arr)) :
-	outarray.append("NaN" if not mask[n] else arr[n])
+	rowstr = rowstr + ("NaN," if not mask[n] else (str(arr[n]) + ","))
+
+	currentCol += 1
+	if currentCol >= int(dimensions[0]) :
+		rowstr = rowstr[:-1] # strip comma and newline
+		rowstr = rowstr + "\n"
+		outstr = outstr + rowstr
+		rowstr = ""
+		currentCol = 0
 
 	minRead = min(minRead, arr[n])
 	maxRead = max(maxRead, arr[n])
@@ -39,6 +57,7 @@ for n in range(len(arr)) :
 print("Min value {0}, Max value {1}".format(minRead, maxRead))
 
 outfile = open(outname, "w")
-outstr = str(outarray)[1:-1] #strip [] from the stringified array
-outfile.write(str(outstr))
+#outstr = str(outarray)[1:-1] #strip [] from the stringified array
+#print(outstr)
+outfile.write(outstr)
 outfile.close()
