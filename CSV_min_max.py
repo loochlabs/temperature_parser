2434287e57e2998c2b08d3de0fb5ab46b262c7b7
ktlearnstocode <katie.coluccio@gmail.com>
2019-08-01 15:02:54 +1200

    Update CSV_min_max.py
    Updated the code for accessing the directory

--- CSV_min_max.py
+++ CSV_min_max.py
@@ -13,6 +13,11 @@ Created on Wed Jul 31 10:45:16 2019
 #		you sort of describe this but I'm adding this to clarify.
 #		See lines 13-15 of cmd_converter.py for this
 
+#KC reply to above: I am a bit confused where you put the directory path (see sys.path... that I've added.)
+#Do you put it before the function or within the defined function? Because from what I understand python by default
+#looks at the current directory. So I need to override that by giving it a directory.
+#This is a pretty key thing I need to understand as I will need to do this all the time. (I.e. access files)
+
 #1) Read 1 CSV
 #2) Get min and max values from that CSV and add that to a list
 #3) Close that CSV
@@ -32,8 +37,11 @@ Created on Wed Jul 31 10:45:16 2019
 
 #%%
 import csv
+import sys
 
-def csv_min_max(filename):
+sys.path.append("C:\Local\OneDrive - University of Canterbury\My Documents\PhD\Data Analysis\TIR\Test Data") #directory where CSVs are saved
+
+def csv_min_max(filename): #Hmm, I'm a bit confused about if you direct the script to look at a whole directory, how does 'filename' fit in here? I.e., I wouldn't direct the function to a specific folder then.
     f = open(filename, 'r') #open file
     content = csv.reader(f, delimiter=',')
 
