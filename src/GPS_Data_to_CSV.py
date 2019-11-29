dc63b80e601b0458da50e432b53057cbbb797acd
Steve Coluccio <thecalooch@gmail.com>
2019-11-28 19:27:50 -0500

    changes to get this conversion working

--- src/GPS_Data_to_CSV.py
+++ src/GPS_Data_to_CSV.py
@@ -16,59 +16,50 @@ file_directory = "C:/Local/OneDrive - University of Canterbury/My Documents/PhD/
 #create a list of the data for the CSV
 csv_list = []
 
+def read_optris(filename) :    
+    #open this file with an appropriate text encoding
+    with open(filename, encoding="latin_1") as fn :
+        content = fn.readlines()
+
+    #grab the line in optris files that matters
+    gpsline = content[2]
+    gpsline = gpsline.replace(" ", "")
+    
+    #Convert a char to an integer
+    gpstrimmed = ""
+    for c in gpsline :
+        num = ord(c)
+        if 9 < num < 100:
+            gpstrimmed += c
+
+    #grab relevant values in the parsed lines
+    gpsarray = gpstrimmed.split(',')[3:7]
+
+    #convert to an appropriate lat/long format
+    latitude = float(gpsarray[0])/100
+    longitude = float(gpsarray[2])/100
+
+    #give our lat/long the correct sign for South, West
+    if gpsarray[1].lower() == 's' :
+        latitude *= -1
+
+    if gpsarray[3].lower() == 'w' :
+        longitude *= -1
+
+    #these are the items that go in the list    
+    return [filename + ".tif", latitude, longitude, "304.8"]
+
 #run Optris reader function
 for filename in os.listdir(file_directory):
+    csv_list.append(read_optris(filename))
     
-    def read_optris(filename) :    
-        #open this file with an appropriate text encoding
-        with open(filename, encoding="latin_1") as fn :
-            content = fn.readlines()
-    
-        #grab the line in optris files that matters
-        gpsline = content[2]
-        gpsline = gpsline.replace(" ", "")
-        
-    	#Convert a char to an integer
-        gpstrimmed = ""
-        for c in gpsline :
-            num = ord(c)
-            if 9 < num < 100:
-                gpstrimmed += c
-    
-    	#grab relevant values in the parsed lines
-        gpsarray = gpstrimmed.split(',')[3:7]
-    
-    	#convert to an appropriate lat/long format
-        latitude = float(gpsarray[0])/100
-        longitude = float(gpsarray[2])/100
-    
-    	#give our lat/long the correct sign for South, West
-        if gpsarray[1].lower() == 's' :
-            latitude *= -1
-    
-        if gpsarray[3].lower() == 'w' :
-            longitude *= -1
-    
-        #these are the items that go in the list    
-        csv_list = [filename + ".tif", latitude, longitude, "304.8"] 
-
-#don't think we need these next few lines right now
-#	print("{0} contains coordinates | (lat:{1}, lng:{2})".format(filename, latitude, longitude))
-#
-#	#return the lat/long as a tuple
-#	return (latitude, longitude)
-        
-       
-
 #update date in filename as needed        
 with open("TIR_GPS_Data_6Nov.csv","w") as new_file:
     fieldnames = ["Filename", "Latitude", "Longitude", "Altitude"]
     csv_writer = csv.DictWriter(new_file,delimiter = ",",fieldnames=fieldnames)
     csv_writer.writeheader()
-    rows = []
     
-#    for row in csv_writer:
+    #for row in csv_writer:
     csv_writer.writerow(csv_list)
-        
-#    new_file.close() #don't think I actually need this.
+    csv_writer.close()
         
